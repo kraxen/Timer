6213244b758c874f5093ffc37e87f952143b1f1c
Виталий <axe211411@gmail.com>
2021-09-14 19:50:16 +0400

    Добавьте файлы проекта.

---
+++ Timer/TimeProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Timer
+{
+    public class TimeProvider: INotifyPropertyChanged
+    {
+        DispatcherTimer timer;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string sTime;
+        public string Time
+        {
+            get { return this.sTime; }
+            set 
+            { 
+                sTime = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Time)));
+            }
+        }
+
+        public TimeProvider()
+        {
+            timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 0, 1)
+            };
+
+            sTime = String.Empty;
+
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            Time = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
